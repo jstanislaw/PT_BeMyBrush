28564d32a67899784af72baa50728a64d8cf5dae
Stanisław J <>
2019-04-10 18:23:48 +0200

    Detection

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import cv2
 import numpy as np
+import imutils
 import time
 
-
 if __name__ == "__main__":
+
+    orangeLower = (8, 150, 115)
+    orangeUpper = (60, 255, 255)
 
     videoInput = cv2.VideoCapture(0)
     time.sleep(1.0)
@@ -12,6 +15,25 @@ if __name__ == "__main__":
         ret, frame = videoInput.read()
 
         frame = np.fliplr(frame)
+        frame = imutils.resize(frame, width=600)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, orangeLower, orangeUpper)
+
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+            if radius > 10:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
