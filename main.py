1ac34734eeddc088be58332a8476bd2eab9052a9
rav97 <35076475+rav97@users.noreply.github.com>
2019-04-19 19:05:25 +0200

    Drawing and Canvas

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ import numpy as np
 import imutils
 import time
 
+def DrawPoint( x, y, radius, blue, green, red, output):
+    cv2.circle(output, (int(x), int(y)), int(radius), (blue, green, red), -1)
+
+def CreateBlankCanvas(width, height, blue, green, red):
+    return np.full((height, width, 3), (blue, green, red), np.uint8)
+
+def DrawLine(ox, oy, nx, ny, thickness, blue, green, red, output):
+    output = cv2.line(output, (int(ox), int(oy)), (int(nx), int(ny)),(blue, green, red), thickness)
+
+
 if __name__ == "__main__":
 
     orangeLower = (8, 150, 115)
     orangeUpper = (20, 255, 241)
+
+    oldX = int(0)
+    oldY = int(0)
+
+    canvas = CreateBlankCanvas(600, 460, 255, 255, 255)
 
     videoInput = cv2.VideoCapture(0)
     time.sleep(1.0)
@@ -18,8 +33,6 @@ if __name__ == "__main__":
         frame = imutils.resize(frame, width=600)
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, orangeLower, orangeUpper)
-
-        #cv2.imshow("mask", mask)
 
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
@@ -37,8 +50,16 @@ if __name__ == "__main__":
             if radius > 10:
                 cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                if (ox == 0 and oy == 0):
+                    DrawPoint(x, y, 6, 255, 0, 0, canvas)
+                else:
+                    DrawLine(oldX, oldY, x, y, 6, 255, 0, 0, canvas)
+            oldX = x
+            oldY = y
 
         cv2.imshow("Frame", frame)
+        cv2.imshow("Canvas", canvas)
+
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
