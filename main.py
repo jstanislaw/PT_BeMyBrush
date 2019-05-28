76f93fca915cc3d8b88baffa9152ea4d4cb1a199
kgrz97 <35100166+kgrz97@users.noreply.github.com>
2019-05-28 18:11:33 +0200

    Update

--- main.py
+++ main.py
@@ -15,13 +15,83 @@ def DrawLine(ox, oy, nx, ny, thickness, blue, green, red, output):
 def nothing(x):
     pass
 
+def WriteToFile(Ol, Ou):
+    ParametersFile = open("Resources/Config.txt", "w")
+
+    ParametersFile.write(str(Ol[0]) + '\n')
+    ParametersFile.write(str(Ol[1]) + '\n')
+    ParametersFile.write(str(Ol[2]) + '\n')
+    ParametersFile.write(str(Ou[0]) + '\n')
+    ParametersFile.write(str(Ou[1]) + '\n')
+    ParametersFile.write(str(Ou[2]) + '\n')
+
+    ParametersFile.close()
+
+def ReadFromFile(nr):
+    ParametersFile = open('Resources/Config.txt')
+    try:
+        tekst = ParametersFile.read()
+    finally:
+        ParametersFile.close()
+
+    x = tekst.split('\n')
+    i=0
+
+    for a in range(6):
+        nr[i] = int(x[i])
+        i=i+1
+
+
 if __name__ == "__main__":
+
+    nr = [0, 0, 0, 0, 0, 0]
+
+    ReadFromFile(nr)
 
     alpha = 0.8
     beta = (1-alpha)
 
-    orangeLower = (8, 150, 115)
-    orangeUpper = (20, 255, 241)
+    orangeLower = (nr[0], nr[1], nr[2])
+    orangeUpper = (nr[3], nr[4], nr[5])
+
+    videoInput2 = cv2.VideoCapture(0)
+    time.sleep(1.0)
+
+    cv2.namedWindow('Frame', 0)
+    cv2.createTrackbar('OL H', 'Frame', orangeLower[0], 179, nothing)
+    cv2.createTrackbar('OL S', 'Frame', orangeLower[1], 255, nothing)
+    cv2.createTrackbar('OL V', 'Frame', orangeLower[2], 255, nothing)
+    cv2.createTrackbar('OU H', 'Frame', orangeUpper[0], 179, nothing)
+    cv2.createTrackbar('OU S', 'Frame', orangeUpper[1], 255, nothing)
+    cv2.createTrackbar('OU V', 'Frame', orangeUpper[2], 255, nothing)
+
+    while True:
+        ret2, frame2 = videoInput2.read()
+
+        frame2 = np.fliplr(frame2)
+        frame2 = imutils.resize(frame2, width=600)
+        hsv2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
+        mask2 = cv2.inRange(hsv2, orangeLower, orangeUpper)
+
+        orangeLower = (cv2.getTrackbarPos('OL H', 'Frame'), cv2.getTrackbarPos('OL S', 'Frame'), cv2.getTrackbarPos('OL V', 'Frame'))
+        orangeUpper = (cv2.getTrackbarPos('OU H', 'Frame'), cv2.getTrackbarPos('OU S', 'Frame'), cv2.getTrackbarPos('OU V', 'Frame'))
+
+
+        cv2.imshow("Frame", mask2)
+
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('\r'): # carriage return
+            break
+
+    videoInput2.release()
+    cv2.destroyWindow('Frame')
+
+    WriteToFile(orangeLower, orangeUpper)
+
+    print(orangeLower)
+    print(orangeUpper)
+
 
     ox = int(0)
     oy = int(0)
@@ -38,7 +108,7 @@ if __name__ == "__main__":
     cv2.createTrackbar('G', 'Frame', 0, 255, nothing)
     cv2.createTrackbar('B', 'Frame', 0, 255, nothing)
     cv2.createTrackbar('Rubber', 'Frame', 0, 1, nothing)
-    cv2.createTrackbar('Brush Size', 'Frame', 10, 40, nothing)
+    cv2.createTrackbar('Brush Size', 'Frame', 1, 40, nothing)
 
     brush_color = [0, 0, 0]
     canvas_color = [0, 111, 230]
@@ -57,7 +127,7 @@ if __name__ == "__main__":
         rubber = cv2.getTrackbarPos('Rubber', 'Frame')
         brush_size = cv2.getTrackbarPos('Brush Size', 'Frame')
 
-        if( tn ):
+        if(tn):
             canvas = CreateBlankCanvas(frame.shape[1], frame.shape[0], canvas_color[0], canvas_color[1], canvas_color[2])
             tn = False
 
@@ -99,7 +169,7 @@ if __name__ == "__main__":
 
         output = cv2.addWeighted(frame, alpha, canvas, beta, 0.0)
         merged1 = np.hstack((output, canvas))
-        merged2 = np.vstack((CreateBlankCanvas(1200, 20, brush_color[0], brush_color[1], brush_color[2]), merged1 ))
+        merged2 = np.vstack((merged1, CreateBlankCanvas(1200, 20, brush_color[0], brush_color[1], brush_color[2])))
 
         cv2.imshow("Frame", merged2)
 
